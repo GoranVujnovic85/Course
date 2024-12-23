aae4f9ebb1b087d3f870897e09041fa848e44cb1
bojanstevanovic <bojanstevanovic@live.com>
2024-12-23 20:37:58 +0100

    bojan

--- GYM/assets/js/spacelocker.js
+++ GYM/assets/js/spacelocker.js
@@ -1,606 +1,6 @@
 
-const data = {
-  results: {
-    "tags": [
-      {
-        "id": 65,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Daily Snack",
-        "type": "packages",
-        "createdAt": "2024-11-07T18:27:37.000Z",
-        "updatedAt": "2024-11-09T00:15:02.000Z"
-      },
-      {
-        "id": 64,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Daily Base",
-        "type": "packages",
-        "createdAt": "2024-11-07T18:27:37.000Z",
-        "updatedAt": "2024-11-07T18:27:37.000Z"
-      },
-      {
-        "id": 63,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Daily Dessert",
-        "type": "packages",
-        "createdAt": "2024-11-07T18:27:37.000Z",
-        "updatedAt": "2024-11-09T00:15:02.000Z"
-      },
-      {
-        "id": 62,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Daily Side",
-        "type": "packages",
-        "createdAt": "2024-11-07T18:27:37.000Z",
-        "updatedAt": "2024-11-07T18:27:37.000Z"
-      },
-      {
-        "id": 61,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Daily Main",
-        "type": "packages",
-        "createdAt": "2024-11-07T18:27:37.000Z",
-        "updatedAt": "2024-11-07T18:27:37.000Z"
-      },
-      {
-        "id": 60,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Dessert",
-        "type": "category",
-        "createdAt": "2024-10-10T09:54:23.000Z",
-        "updatedAt": "2024-10-25T00:15:01.000Z"
-      },
-      {
-        "id": 59,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Side",
-        "type": "category",
-        "createdAt": "2024-10-10T09:54:11.000Z",
-        "updatedAt": "2024-10-19T00:15:02.000Z"
-      },
-      {
-        "id": 58,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Main",
-        "type": "category",
-        "createdAt": "2024-10-10T09:54:07.000Z",
-        "updatedAt": "2024-10-19T00:15:02.000Z"
-      },
-      {
-        "id": 57,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Base",
-        "type": "category",
-        "createdAt": "2024-10-10T09:54:01.000Z",
-        "updatedAt": "2024-10-23T19:39:29.000Z"
-      },
-      {
-        "id": 56,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Snack",
-        "type": "category",
-        "createdAt": "2024-10-10T09:53:55.000Z",
-        "updatedAt": "2024-11-05T00:15:02.000Z"
-      },
-      {
-        "id": 55,
-        "is_active": true,
-        "is_linked": false,
-        "name": "Bronze Sandwiches",
-        "type": "packages",
-        "createdAt": "2024-10-10T09:53:34.000Z",
-        "updatedAt": "2024-10-10T09:53:34.000Z"
-      },
-      {
-        "id": 54,
-        "is_active": true,
-        "is_linked": false,
-        "name": "Bronze",
-        "type": "packages",
-        "createdAt": "2024-10-10T09:53:15.000Z",
-        "updatedAt": "2024-10-10T09:53:15.000Z"
-      },
-      {
-        "id": 53,
-        "is_active": true,
-        "is_linked": false,
-        "name": "Silver",
-        "type": "packages",
-        "createdAt": "2024-10-10T09:53:08.000Z",
-        "updatedAt": "2024-10-10T09:53:08.000Z"
-      },
-      {
-        "id": 52,
-        "is_active": true,
-        "is_linked": false,
-        "name": "Gold",
-        "type": "packages",
-        "createdAt": "2024-10-10T09:53:01.000Z",
-        "updatedAt": "2024-10-10T09:53:01.000Z"
-      },
-      {
-        "id": 34,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Product Launch",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 33,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Team Building",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 35,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Training Session",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 36,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Networking Event",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 37,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Summer Party",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 38,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Seasonal Party",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 39,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Eid",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 40,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Diwali",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 41,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Halloween",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 42,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Christmas",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 43,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Valentines",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 44,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Thanksgiving",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 45,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Birthdays",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 46,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Chinese New Year",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 47,
-        "is_active": false,
-        "is_linked": true,
-        "name": "St. Patrick Day",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 48,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Easter",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 49,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Mothers Day",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 50,
-        "is_active": true,
-        "is_linked": true,
-        "name": "New Year",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 51,
-        "is_active": false,
-        "is_linked": true,
-        "name": "International Womans Day",
-        "type": "calendar",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 32,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Fundraiser",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 11,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Organic",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 12,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Canapés",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 13,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Desserts",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 14,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Snacks",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 15,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Finger Food",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 16,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Afternoon Tea",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 17,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Dinner",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 18,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Breakfast",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 19,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Lunch",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 20,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Platters",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 21,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Bundles",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 23,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Pastries",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 24,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Cakes",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 25,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Cocktails / Bar",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 26,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Tea / Coffee",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 27,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Corporate Event",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 28,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Meeting",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 29,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Celebration",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 30,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Treat",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 31,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Workshop",
-        "type": "event",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 22,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Sweets",
-        "type": "category",
-        "createdAt": "2024-04-16T05:44:23.000Z",
-        "updatedAt": "2024-04-16T05:44:23.000Z"
-      },
-      {
-        "id": 2,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Vegan",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-04-16T05:44:22.000Z"
-      },
-      {
-        "id": 3,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Gluten-free",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-04-16T05:44:22.000Z"
-      },
-      {
-        "id": 4,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Nut-Free",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-04-16T05:44:22.000Z"
-      },
-      {
-        "id": 5,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Halal",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 6,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Kosher",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 7,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Dairy-Free",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-04-16T05:44:22.000Z"
-      },
-      {
-        "id": 8,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Low-Carb",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-04-16T05:44:22.000Z"
-      },
-      {
-        "id": 9,
-        "is_active": true,
-        "is_linked": true,
-        "name": "High-Protein",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-06-24T04:24:06.000Z"
-      },
-      {
-        "id": 10,
-        "is_active": true,
-        "is_linked": true,
-        "name": "Keto",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:22.000Z",
-        "updatedAt": "2024-04-16T05:44:22.000Z"
-      },
-      {
-        "id": 1,
-        "is_active": false,
-        "is_linked": true,
-        "name": "Vegetarian",
-        "type": "dietry",
-        "createdAt": "2024-04-16T05:44:21.000Z",
-        "updatedAt": "2024-04-16T05:44:21.000Z"
-      }
-    ],
-    "paginate": {
-      "total": 65,
-      "limit": 100,
-      "offset": 0,
-      "order": [
-        [
-          "createdAt",
-          "DESC"
-        ]
-      ]
-    }
-  }
-}
+let aa11 = null
+
 // Define translations object with default values for English and Serbian
 const translations = {
   en: {
@@ -621,10 +21,11 @@ const translations = {
   }
 };
 
+
 // Create LOCKER object as an instance of lockerClass
 var LOCKER = new lockerClass();
 
-function lockerClass() {
+function lockerClass(data) {
   // Function to change language
   this.changeLanguage = function (language) {
     // Store the selected language in localStorage
@@ -657,7 +58,6 @@ function lockerClass() {
   // Locker function to calculate count and update the result display
   this.locker = function (data) {
     console.log("Running locker function..."); // Debugging line
-
     let count = 0;
     data.results.tags.forEach(tag => {
       if (tag.is_active === false) {
@@ -701,6 +101,7 @@ function lockerClass() {
 
   // Function to display inactive IDs in the input field
   this.displayInactiveIds = function () {
+    const data = aa11;
     const inactiveIds = this.getInactiveIdsFromTags(data);
     const resultDisplay = document.getElementById('resultDisplay2');
 
@@ -744,6 +145,7 @@ function lockerClass() {
     const tags = data.results.tags;
     const activeTags = tags.filter(tag => activeIds.includes(tag.id));
     activeTags.sort((a, b) => a.id - b.id);
+    console.log("Inactive IDs:", activeIds); // Log inactive IDs
 
     const container = document.querySelector('.data-container');
     container.innerHTML = '';
@@ -820,3 +222,35 @@ function lockerClass() {
     LOCKER.displayInactiveIds();
   });
 }
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+  try {
+    debugger
+    // URL to your JSON file with a timestamp to avoid caching
+    const jsonUrl = `http://localhost/GYM/assets/json/locker.json`;
+
+    // Loading the JSON file
+    const response = await fetch(jsonUrl);
+    console.log("fetck OK");
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    aa11 = data;
+
+    // Initialize the LOCKER object after data is fetched
+    LOCKER = new lockerClass(data);  // Pass data to the lockerClass constructor
+
+    // Call locker and datarows after the LOCKER instance is ready
+    LOCKER.updateContent(localStorage.getItem('selectedLanguage') || 'en');
+    
+    LOCKER.displayInactiveIds();
+
+  } catch (error) {
+    console.error('Error loading JSON:', error);
+  }
+});
